e5102b6d7f956be433075c6a7078496c049d4eb3
liuhuiwen <1305156911@qq.com>
2025-05-29 10:28:10 +0800

    refactor(websocket): 重构 WebSocketInterceptor 类
    - 移除了 unused 的 import 语句
    - 添加了 lombok.extern.slf4j.Slf4j 注解用于日志记录- 重命名了方法参数以提高可读性
    - 删除了未使用的 token 相关代码- 在 afterHandshake 方法中添加了用户连接成功的日志记录

--- src/main/java/cn/liulingfengyu/websocket/core/conf/WebSocketInterceptor.java
+++ src/main/java/cn/liulingfengyu/websocket/core/conf/WebSocketInterceptor.java
@@ -1,16 +1,14 @@
 package cn.liulingfengyu.websocket.core.conf;
 
 import cn.hutool.core.lang.UUID;
-import cn.hutool.core.util.StrUtil;
 import jakarta.annotation.Nonnull;
-import org.apache.tomcat.util.buf.UDecoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
-import java.nio.charset.Charset;
 import java.util.Map;
 
 /**
@@ -18,22 +16,17 @@ import java.util.Map;
  *
  * @author LLFY
  */
+@Slf4j
 public class WebSocketInterceptor implements HandshakeInterceptor {
 
     private static final String USER_ID = "userId";
 
-    private static final String TOKEN = "token";
-
     @Override
-    public boolean beforeHandshake(@Nonnull ServerHttpRequest request,
+    public boolean beforeHandshake(@Nonnull ServerHttpRequest serverHttpRequest,
                                    @Nonnull ServerHttpResponse serverHttpResponse,
                                    @Nonnull WebSocketHandler webSocketHandler,
-                                   @Nonnull Map<String, Object> map) throws Exception {
-        if (request instanceof ServletServerHttpRequest servletServerHttpRequest) {
-            String token = UDecoder.URLDecode(servletServerHttpRequest.getServletRequest().getParameter(TOKEN), Charset.defaultCharset());
-            if (StrUtil.isBlank(token)) {
-                throw new Exception("未登录");
-            }
+                                   @Nonnull Map<String, Object> map) {
+        if (serverHttpRequest instanceof ServletServerHttpRequest servletServerHttpRequest) {
             map.put(USER_ID, servletServerHttpRequest.getServletRequest().getParameter(USER_ID).concat("-").concat(UUID.randomUUID().toString(true)));
             return true;
         }
@@ -44,5 +37,9 @@ public class WebSocketInterceptor implements HandshakeInterceptor {
     public void afterHandshake(@Nonnull ServerHttpRequest serverHttpRequest,
                                @Nonnull ServerHttpResponse serverHttpResponse,
                                @Nonnull WebSocketHandler webSocketHandler, Exception e) {
+        if (serverHttpRequest instanceof ServletServerHttpRequest servletServerHttpRequest) {
+            String userId = servletServerHttpRequest.getServletRequest().getParameter(USER_ID);
+            log.info("用户{}链接成功", userId);
+        }
     }
 }
